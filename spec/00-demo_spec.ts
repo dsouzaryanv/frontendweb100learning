f59eb4bd895588a2b8e87614b6c0a663c8b6e601
Ryan Dsouza <dsouzaryanv@gmail.com>
2019-02-19 11:24:23 -0500

    initial test checkin

---
+++ spec/00-demo_spec.ts
@@ -0,0 +1,11 @@
+describe('Writing a basic spec', () => {
+    it('should be easy', () => {
+        //Given (arrange)
+        let firstName = 'Boba',
+            lastName = 'Fett';
+        //When (act)
+        let fullName = lastName + ', ' + firstName;
+        //Then (assert)
+        expect(fullName).toBe('Fett, Boba');
+    });
+});
