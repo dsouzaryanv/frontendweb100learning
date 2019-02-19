08658e396deece3dabb415e6de75c49c651fb6dd
Ryan Dsouza <dsouzaryanv@gmail.com>
2019-02-19 15:52:03 -0500

    more changes

--- spec/02-functions_spec.ts
+++ spec/02-functions_spec.ts
@@ -1,3 +1,6 @@
+import { formatName, formatName2 } from "./utils";
+import * as _ from 'lodash';
+
 describe('functions', () => {
     describe('syntax for creating them', () => {
         it('declaring them', () => {
@@ -28,10 +31,91 @@ describe('functions', () => {
         describe('higher order functions', () => {
             //A higher order function is a function that takes an an argument one or more functions and/or returns a fucntion
             it('first example', () => {
+                const answer = formatName('Han', 'Solo', makeItUpper);
+                expect(answer).toBe('SOLO, HAN');
 
+                const answer2 = formatName2('Han', "Solo", (x: string) => `***${x}***`)
+                expect(answer2).toBe('***Solo, Han***');
 
+                function makeItUpper(what: string) {
+                    return what.toUpperCase();
+                }
             });
         });
 
+        describe('a function that returns a function', () => {
+            it('a way to do it that does not crazy', () => {
+                //<h1>Hello</h1>
+                //tag, content
+                //straighta head procedural function thing
+                function makeElement(tag: string, content: string) {
+                    return `<${tag}>${content}</${tag}>`;
+                }
+                expect(makeElement('h1', 'Hello')).toBe('<h1>Hello</h1>');
+                expect(makeElement('p', 'the story')).toBe('<p>the story</p>');
+            });
+            it('you could try OOP', () => {
+                class ElementMaker {
+                    tag: string;
+                    constructor(tag: string) {
+                        this.tag = tag;
+                    }
+                    make(content: string) {
+                        return `<${this.tag}>${content}</${this.tag}>`;
+                    }
+                }
+                const h1Maker = new ElementMaker('h1');
+                expect(h1Maker.make('hello')).toBe('<h1>hello</h1>');
+                expect(h1Maker.make('big!')).toBe('<h1>big!</h1>');
+            });
+            it('a higher order function version ', () => {
+                function tagMaker(tag: string) {
+                    return (content: string) => `<${tag}>${content}</${tag}>`;
+                }
+                const h1Maker = tagMaker('h1');
+                const pMaker = tagMaker('p');
+
+                expect(h1Maker('big!')).toBe('<h1>big!</h1>');
+                expect(pMaker('small')).toBe('<p>small</p>');
+            });
+        });
+
+    });
+
+    describe('some lodash goodness', () => {
+        it('supports memoization', () => {
+            function doSomeHardWork() {
+                console.log('Doing some hard work here ');
+                return 'Work is Done';
+            }
+            const memo = _.memoize(doSomeHardWork);
+            expect(memo()).toBe('Work is Done');
+            expect(memo()).toBe('Work is Done');
+            expect(memo()).toBe('Work is Done');
+            expect(memo()).toBe('Work is Done');
+            expect(memo()).toBe('Work is Done');
+            expect(memo()).toBe('Work is Done');
+            expect(memo()).toBe('Work is Done');
+        });
+
+        it('currying', () => {
+            function makeElement(tag: string, content: string) {
+                return `<${tag}>${content}</${tag}>`;
+            }
+            const curriedTagMaker = _.curry(makeElement);
+            const h1Maker = curriedTagMaker('h1');
+            const pMaker = curriedTagMaker('p');
+
+            expect(h1Maker('HELLO')).toBe('<h1>HELLO</h1>');
+            expect(pMaker('see you later')).toBe('<p>see you later</p>');
+
+            function add(a: number, b: number, c: number) {
+                return a + b + c;
+            }
+
+            const curriedAdd = _.curry(add);
+            const f1 = curriedAdd(10, 5);
+            expect(f1(2)).toBe(17);
+        });
     });
 });
--- spec/utils.ts
+++ spec/utils.ts
@@ -1,3 +1,8 @@
-function formatName(first: string, last: string) {
-    return `${last}, ${first}`;
+export function formatName(first: string, last: string, fn: any) {
+    return fn(`${last}, ${first}`);
 }
+
+export function formatName2(first: string, last: string, fn: (x: string) => string) {
+    return fn(`${last}, ${first}`);
+}
+
